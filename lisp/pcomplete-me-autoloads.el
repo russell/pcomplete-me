092447e8854947a1dc2b49b29c12c555fab06608
Russell Sim <russell.sim@gmail.com>
2021-07-18 11:41:12 +0200

    updated autoloads

--- lisp/pcomplete-me-autoloads.el
+++ lisp/pcomplete-me-autoloads.el
@@ -1,15 +1,30 @@
-;;; pcomplete-me-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
+;;; pcomplete-me-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
 
+
+;;;### (autoloads nil "bash-completion-export-utils" "bash-completion-export-utils.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from bash-completion-export-utils.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "bash-completion-export-utils" '("rs//")))
+
+;;;***
 
 ;;;### (autoloads nil "pcmpl-kubectl" "pcmpl-kubectl.el" (0 0 0 0))
 ;;; Generated autoloads from pcmpl-kubectl.el
 
 (autoload 'pcomplete/kubectl "pcmpl-kubectl" "\
-Completion for `kubectl'" nil nil)
+Completion for kubectl." nil nil)
 
-(register-definition-prefixes "pcmpl-kubectl" '("kubectl-output-" "pcmpl-kubectl-" "rs//"))
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "pcmpl-kubectl" '("kubectl-output-" "pcmpl-kubectl-")))
+
+;;;***
+
+;;;### (autoloads nil "pcomplete-me" "pcomplete-me.el" (0 0 0 0))
+;;; Generated autoloads from pcomplete-me.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "pcomplete-me" '("pcmpl-me-")))
 
 ;;;***
 
