17dfc0f77b5c25d769dc03fae19c7976278d20c6
Russell Sim <russell.sim@gmail.com>
2021-07-27 08:06:47 +0200

    chore: remove autoloads

--- lisp/pcomplete-me-autoloads.el
+++
@@ -1,38 +0,0 @@
-;;; pcomplete-me-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "bash-completion-export-utils" "bash-completion-export-utils.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from bash-completion-export-utils.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "bash-completion-export-utils" '("rs//")))
-
-;;;***
-
-;;;### (autoloads nil "pcmpl-kubectl" "pcmpl-kubectl.el" (0 0 0 0))
-;;; Generated autoloads from pcmpl-kubectl.el
-
-(autoload 'pcomplete/kubectl "pcmpl-kubectl" "\
-Completion for kubectl." nil nil)
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "pcmpl-kubectl" '("kubectl-output-" "pcmpl-kubectl-")))
-
-;;;***
-
-;;;### (autoloads nil "pcomplete-me" "pcomplete-me.el" (0 0 0 0))
-;;; Generated autoloads from pcomplete-me.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "pcomplete-me" '("pcmpl-me-")))
-
-;;;***
-
-(provide 'pcomplete-me-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; pcomplete-me-autoloads.el ends here
